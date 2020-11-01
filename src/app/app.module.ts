2b9e959cd355eb49f5fef88587b4350e79b8c285
prasmik24 <prashantjsr03@gmail.com>
2020-11-01 18:45:09 +0530

    Front End

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/shared/header/header.component';
-import { FooterComponent } from './components/shared/footer/footer.component';
-import { NavComponent } from './components/shared/nav/nav.component';
-import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 import { ProductInfoComponent } from './components/product-info/product-info.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { OffersComponent } from './components/offers/offers.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { CartComponent } from './components/cart/cart.component';
+import { OrderconfirmationComponent } from './components/orderconfirmation/orderconfirmation.component';
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
+import { ProductdescriptionComponent } from './components/productdescription/productdescription.component';
+
+const appRoutes:Routes=[
+  {path:'',component:HomeComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'products',component:ProductInfoComponent},
+  {path:'offers',component:OffersComponent},
+  {path:'checkout',component:CheckoutComponent},
+  {path:'cart',component:CartComponent},
+  {path:'resetpassword',component:ResetpasswordComponent},
+  {path:'productdescription',component:ProductdescriptionComponent},
+  {path:'cart',component:CartComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    NavComponent,
-    ShoppingCartComponent,
     ProductInfoComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent,
+    OffersComponent,
+    CheckoutComponent,
+    CartComponent,
+    OrderconfirmationComponent,
+    ResetpasswordComponent,
+    ProductdescriptionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/productdescription/productdescription.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productdescription',
+  templateUrl: './productdescription.component.html',
+  styleUrls: ['./productdescription.component.css']
+})
+export class ProductdescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
